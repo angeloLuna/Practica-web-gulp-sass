70d5de6280785063fc629eeceeb08620da9664be
angel.luna.najera@gmail.com <angel.luna.najera@gmail.com>
2017-06-13 14:54:03 -0500

    agregadas tareas copy y sass de gulp, index, y styles.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('src/assets/style/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('src/assets/style'))
+})
+
+gulp.task('copy', function(){
+  gulp.src('src/**/*')
+  .pipe(gulp.dest('dist/'))
+})
